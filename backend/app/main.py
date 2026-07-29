b05f6ee889085206fdb334e3dae122814f41c2c5
Sumit Kumar Sharma <sumitkumarsharma008@gmail.com>
2026-07-29 18:31:39 +0530

    Modify CORS settings to include new origin
    Updated CORS middleware to allow new origins.

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,8 +9,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
+        "http://16.192.127.14:8080/",
+        "http://16.192.127.14:8080/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
