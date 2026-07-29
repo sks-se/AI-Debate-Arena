18055d773b7ac01f3cd01650068c98f6c3ecbf14
Sumit Kumar Sharma <sumitkumarsharma008@gmail.com>
2026-07-29 18:47:26 +0530

    Modify CORS settings for FastAPI application
    Updated CORS middleware configuration to allow all origins and disabled credentials.

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,11 +8,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://16.192.127.14:8080/",
-        "http://16.192.127.14:8080/",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
